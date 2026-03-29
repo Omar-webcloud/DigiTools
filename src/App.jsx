fbf7ac9322155baa1f0dc7fee9d9886911eba710
Md Omar Faruk Chowdhury <ofcjim16@gmail.com>
2026-03-29 21:23:53 +0600

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import { useState } from 'react'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center p-4">
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" className="rounded-xl" />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h1 className="card-title text-3xl font-bold">Tech Stack Ready!</h1>
-          <p className="py-4">React.js + Tailwind CSS + DaisyUI</p>
-          <div className="card-actions">
-            <button 
-              className="btn btn-primary" 
-              onClick={() => setCount((count) => count + 1)}
-            >
-              Count is {count}
-            </button>
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-base-100 text-base-content">
+      <h1 className="text-5xl font-bold">digitools Project</h1>
+      <p className="mt-4 text-lg">products</p>
     </div>
   )
 }
